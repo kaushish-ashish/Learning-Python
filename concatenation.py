29fc2702039b3a73df19a55bd7b0980127aad141
kaushish-ashish <kaushish.ashish@gmail.com>
2025-12-03 10:20:00 +0530

    Day 3

--- concatenation.py
+++ concatenation.py
@@ -1,4 +1,4 @@
-#Concatenation   +
+# Concatenation   +
 
 print(10+10)  #20 valid
 print(10.5+12.0) # 22.5 valid
@@ -12,4 +12,5 @@ print(True+True)  # 2 valid
 
 print(10+'welcome') #Not valid bcoz 2 values are different type
 print(10.5+'welcome') #Not valid bcoz 2 values are different type
+
 print(True+'welcome')  #Not valid bcoz 2 values are different type
