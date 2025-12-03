5684c6b7c81c2b6be9501057a9bd2b1f2bf5e486
kaushish-ashish <kaushish.ashish@gmail.com>
2025-12-03 10:27:54 +0530

    Day 2

--- keywords.py
+++ keywords.py
@@ -1,7 +1,9 @@
+# Keywords
 import keyword
 print(keyword.kwlist)  # prints all avaialable keywords in python
 
 '''
 ['False', 'None', 'True', '__peg_parser__', 'and', 'as', 'assert', 
 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 
+
 '''
