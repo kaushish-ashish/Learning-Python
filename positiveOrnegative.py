8c0285386446f998c1728e870a295e868261a1a0
kaushish-ashish <kaushish.ashish@gmail.com>
2025-12-03 10:17:35 +0530

    Day 3

--- positiveOrnegative.py
+++ positiveOrnegative.py
@@ -1,3 +1,4 @@
+# Check the given number is positive or negative
 num=int(input("Enter a number:"))
 if num>0:
     print("positive")
@@ -5,5 +6,6 @@ elif num<0:
     print("negative")
 else:
     print("number is zero")
+
 
 #print("Positive Number" if num > 0 else "Negative Number" if num < 0 else "Number is Zero")
