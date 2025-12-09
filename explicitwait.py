76e35428b098729a02d44be43822bb0f42016a46
kaushish-ashish <kaushish.ashish@gmail.com>
2025-12-09 13:27:58 +0530

    Day 16
    Refactor explicit wait implementation and add Chrome options.

--- explicitwait.py
+++ explicitwait.py
@@ -1,17 +1,24 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.wait import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, ElementNotVisibleException, ElementNotSelectableException
 from selenium.webdriver.support import expected_conditions as EC
 
+chrome_options = Options()
+chrome_options.add_argument("--disable-notifications")
 
-serv_obj=Service("C:\Drivers\chromedriver_win32\chromedriver.exe")
-driver=webdriver.Chrome(service=serv_obj)
+# Initialize driver with options
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()),
+    options=chrome_options
+)
+
 
 #mywait=WebDriverWait(driver,10) # explicit wait declaration # basic
-mywait=WebDriverWait(driver,10,poll_frequency=2,ignored_exceptions=[NoSuchElementException,
+my_wait=WebDriverWait(driver,10,poll_frequency=2,ignored_exceptions=[NoSuchElementException,
                                                    ElementNotVisibleException,
                                                    ElementNotSelectableException,
                                                    Exception]
@@ -21,13 +28,13 @@ mywait=WebDriverWait(driver,10,poll_frequency=2,ignored_exceptions=[NoSuchElemen
 driver.get("https://www.google.com/")
 driver.maximize_window()
 
-searchbox=driver.find_element(By.NAME,'q')
+search_box=driver.find_element(By.NAME,'q')
 
-searchbox.send_keys("Selenium")
-searchbox.submit()
+search_box.send_keys("Selenium")
+search_box.submit()
 
-searchlink=mywait.until(EC.presence_of_element_located((By.XPATH,"//h3[text()='Selenium']")))
-searchlink.click()
+search_link=my_wait.until(EC.presence_of_element_located((By.XPATH,"//h3[text()='Selenium']")))
+search_link.click()
 
 
 driver.quit()
